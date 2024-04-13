16086e6dd3fd511946d9a190c0dbb972251308a2
Ki11akrokBg <kikouzunow@gmail.com>
2024-04-13 15:02:38 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world-2201681069';
+ 
+  items =  [ 
+    { title: "Информация за вас", description: "Име: Кирил Узунов Години: 20 " , expanded: false},
+    { title: "Хобита и интереси", description: "Пътуване, разходки, фитнес, нарга , бизнес и личностно развитие, крипто , инвестиции ", expanded: false},
+    { title: "Link към канал на любим контент криейтър ", description: "https://www.youtube.com/@Georgi_Shishkov | https://www.youtube.com/@kristiankirilov1355", expanded: false},
+    { title: "Информация за вашия канал", description: " Уча хората да бъдат финансово независими и да правят по 1милион на ден с крипто трейдване на миим коинс ", expanded: false},
+    { title: "Пример за оствойностяване", description: " Давам много полезна информация нова за българската аудитория  ", expanded: false},
+
+  ];
+
+    toggle(item : any) {
+      this.items.forEach(i => {
+        if(i !== item) i.expanded = false;
+    });
+      item.expanded = !item.expanded;
+    }
+
 }
+
+
